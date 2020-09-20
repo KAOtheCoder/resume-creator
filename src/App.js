3a764026f5e34c09b0f5346c00a849a8cea75f5f
Kaan-Ucar <kaanucar48@gmail.com>
2020-09-20 19:40:51 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
+import jsPDF from 'jspdf';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <button className="App-button"
+          onClick={() => {
+            var doc = new jsPDF();
+            doc.text(35, 25, 'Pdf Generated.');
+            doc.output('dataurlnewwindow');
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          "Generate pdf"
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
